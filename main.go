a713a060f27a0a659c535a9cedef577e2e0df052
Daniel Marzini <daniel.marzini@iubenda.com>
2019-06-05 10:26:47 +0200

    update

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	if os.Getenv("REDIRECT_DESTINATION") == "" {
+		fmt.Println("please initialize env REDIRECT_DESTINATION")
+		os.Exit(1)
+	}
+}
+
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	r.GET("/*", func(ctx *gin.Context) {
+	r.NoRoute(func(ctx *gin.Context) {
 		ctx.Redirect(http.StatusMovedPermanently, os.Getenv("REDIRECT_DESTINATION"))
 	})
 	r.Run()
